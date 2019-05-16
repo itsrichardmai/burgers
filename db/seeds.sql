df32f11dddf597f967bc1f805ec95a0fd1365550
Richard Mai <richardmai@Richards-MacBook-Pro.local>
2019-05-15 22:44:13 -0400

    gg

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+    INSERT INTO burgers (burger_name, devoured) VALUES ("Shen Burger", false);
+    INSERT INTO burgers (burger_name, devoured) VALUES ("Ha Burger", false);
+    INSERT INTO burgers (burger_name, devoured) VALUES ("Qaz Burger", false);
+    INSERT INTO burgers (burger_name, devoured) VALUES ("Jaop Burger", false);
+    INSERT INTO burgers (burger_name, devoured) VALUES ("Kol Burger", false);
